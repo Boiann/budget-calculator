5a4d8556653d1e4dd378a34372ca10ffe13f93e3
Bojan Bakaric <bakaricbojan@gmail.com>
2023-01-07 09:33:26 +0000

    Add plan to script.js a&footer to html

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/*
+Funcions needed:
+
+- Add event listeners to clculate budget and reset form
+-Calculate budget
+-Reset form
+-Have the option of adding more expense input fields
+-Have the option of adding more income input fields
+-Have the currency be selected and displayed in the final result (possibly use dropdown menu)
+-Have the inputs be mandatory, add warnings if the inputs are empty
+-Have a message display for the user after name input
+
+Extra bits, add if possible:
+
+- Add date to the page
+- Add print option
+- Add some sort of visual representation of the calculated budget ( look into charts, API-s etc)
+- Option to store the data locally ( remember the budget when the user returns)
+*/
+
+// Wait for the DOM to load before running Javascript
+document.addEventListener('DOMContentLoaded', function() {
+    
+  });
